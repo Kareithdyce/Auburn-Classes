ae977228b02029272f77c18554d352ea3751b552
kareithdyce <Kareith.dyce@gmail.com>
2018-12-05 10:47:08 -0600

    Progress has been made lol

--- OS/Project6/vm_sim.cpp
+++ OS/Project6/vm_sim.cpp
@@ -5,30 +5,101 @@
 #include <cstdlib>
 
 using namespace std;
+ /*TLB and pagetable*/
+tlb_t tlb;
+page_table_t pageTable;
 
-int init_TLB(tlb_t &TLB){
-    TLB.next_tlb_ptr = 0;
+
+/* TLB */
+int initTLB(tlb_t &t){
+    unsigned int i;
+    t.next_tlb_ptr = 0;
+    for(i = 0; i < TLB_SIZE; i++){
+        t.tlb_entry[i].valid = false;
+    } 
     return 0;
 } 
 
+int TLB_display(tlb_t t){
+    unsigned int i;
+    for(int i = 0; i < TLB_SIZE; i++){
+        printf("TLB entry %d, page num: %d, frame num: %d, ", i,
+        t.tlb_entry[i].page_num, t.tlb_entry[i].frame_num);
+        
+        if(t.tlb_entry[i].valid){
+           printf("Invalid\n"); 
+        }else{
+            printf("Valid\n");
+        }
+        return 0;
+    }
+}
 
-int init_page_table(page_table_t &pageTable){
-    pageTable.next_ptr = 0;
+int TLB_replacement_FIFO(page_t page, frame_t frame, tlb_t &t){
+    t.tlb_entry[t.next_tlb_ptr].page_num = page;
+    t.tlb_entry[t.next_tlb_ptr].frame_num = frame;
+    t.next_tlb_ptr = (t.next_tlb_ptr+1 % TLB_SIZE);
+    return 0;
+}
+
+int searchTLB(page_t page, tlb_t t, frame_t &frame){
+    int i = 0;
+    for(i; i < TLB_SIZE; i++){
+        if(!t.tlb_entry[i].valid){
+            break;
+        }
+        if(t.tlb_entry[i].page_num == page){
+            frame = t.tlb_entry[i].frame_num;
+            return 0;
+        }
+    }
+    search_pageTable(page, pageTable, frame);
+    return 0;
+}
+
+
+
+/* Page Table*/
+int init_pageTable(page_table_t &pageTable){
     return 0; 
 } 
 
-int logic_address_loader(string file, laddress_list_t &laddress_list){
-    ifstream read;
-    read.open((char*)file.c_str());
-    if(read.fail()){
-        cout<<"Error!"<<endl;
-        return -1;
 
+int search_pageTable(page_t page, page_table_t table, frame_t &frame){
+    int i = 0;
+    for(i; i < PAGE_SIZE; i++){
+        if(table.pages[i] == page){
+            //frame = tlb.tlb_entry[i].frame_num;
+            return 0;
+        }
     }
     return 0;
 }
 
 
+/* Logic Address */
+int laddressLoader(string file, laddress_list_t &list){
+    ifstream read;
+    read.open((char*)file.c_str());
+    if(read.fail()){
+        cout<<"Error!"<<endl;
+        return -1;
+    }
+    list.size = 0;
+    int value;
+    while(read>>value){
+        list.laddress[list.size] = value;
+        list.size++;
+    }
+    return 0;
+}
+
+
+int extract_laddress(laddress_t laddress, page_t &page, offset_t &offset){
+    page = laddress >> OFFSET_BITS;
+    offset = laddress & OFFSET_MASK;
+    return 0;
+}
 
 
 int main(){
@@ -42,10 +113,7 @@ int main(){
     laddress_t laddress;
     laddress_list_t laddressList;
     
-    /*TLB and pagetable*/
-    tlb_t tlb;
-    page_table_t page_table;
-
+   
     //physical_memory_t physical_memory;
     
     /*Value and address-value list*/
@@ -57,16 +125,20 @@ int main(){
     bool is_page_fault;
     
     /*Input and Output file names*/
-    string input_file = "input_logical_address_file";
+    string input_file = "testinput.txt";
     string output_file = "output_physical_address_value";
 
     /*Initialize the system*/
-    init_TLB(tlb);
-    init_page_table(page_table);
+    initTLB(tlb);
+    init_pageTable(pageTable);
 
     /* Create a logical address list from the file*/
-    logic_address_loader(input_file, laddressList);
-    
+    laddressLoader(input_file, laddressList);
+    int i = 0;
+    for(i; i < TLB_SIZE; i++){
+        extract_laddress(laddressList.laddress[i], page_num, offset);
+        searchTLB(page_num, tlb, frame_num);
+    }
 
     
 
--- OS/Project6/vm_sim.h
+++ OS/Project6/vm_sim.h
@@ -6,8 +6,8 @@
 #define PAGE_SIZE 256
 #define TLB_SIZE 16
 #define PHYS_MEM 65536
+#define backing_store "BACKING_STORE"
 using namespace std;
-
 
 typedef unsigned int laddress_t;
 typedef unsigned int paddress_t;
@@ -16,6 +16,7 @@ typedef unsigned int page_t;
 typedef unsigned int frame_t;
 typedef unsigned int offset_t;
 
+typedef unsigned char byte_t;
 
 typedef struct{
     page_t page_num;
@@ -31,35 +32,35 @@ typedef struct {
 
 
 typedef struct {    
-    laddress_t laddress[TLB_SIZE];
+    laddress_t laddress[NUM_PAGES];
     /* next candidate entry to be replaced/used */
-    unsigned int next_ptr;
+    unsigned int size;
 }laddress_list_t;
 
 
 typedef struct {    
-    page_t page[NUM_PAGES];
+    page_t pages[NUM_PAGES];
     /* next candidate entry to be replaced/used */
     unsigned int next_ptr;
 }page_table_t;
 
-int init_TLB(tlb_t &tlb);
-int init_page_table(page_table_t &pageTable);
-
-int TLB_replacement_FIFO(page_t page_num, frame_t frame_num, tlb_t* tlb);
-
+/* TLB */
+int initTLB(tlb_t &t);
+int searchTLB(page_t page, tlb_t t, frame_t &frame);
 int TLB_display(tlb_t tlb);
+int TLB_replacement_FIFO(page_t page_num, frame_t frame_num, tlb_t  &t);
 
+/* Page Table*/
+int init_pageTable(page_table_t &pageTable);
+int search_pageTable(page_t page, page_table_t table, frame_t &frame_num);
+
+/* Logic Address*/
+int laddressLoader(string file, laddress_list_t &list);
 int extract_laddress(laddress_t laddress, page_t &page_num, offset_t &offset);
 
-int search_TLB(page_t page_num, tlb_t tlb, frame_t frame);
-
-int search_pageTable(page_t page_num, tlb_t tlb, frame_t &frame_num);
-
-int page_faultHandler(page_t page_num, tlb_t &tlb);
+int pageFaultHandler(page_t page_num, tlb_t &t);
                                                                                                                              
-int create_paddress(frame_t frame_num, offset_t offset, paddress_t paddress);
-
+/* Physical Address*/
+int create_paddress(frame_t frame_num, offset_t offset, paddress_t &paddress);
 int read_paddress(paddress_t paddress, int value);
 
-int logic_address_loader(string file, laddress_list_t &laddress_list);
