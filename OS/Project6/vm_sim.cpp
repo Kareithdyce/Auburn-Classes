42d4c59c8ba49c043fefa1525bad03399385a3df
kareithdyce <Kareith.dyce@gmail.com>
2018-12-06 16:58:52 -0600

    Some Updates to Project6

--- OS/Project6/vm_sim.cpp
+++ OS/Project6/vm_sim.cpp
@@ -1,14 +1,22 @@
+/** 
+ * Project 6
+ * This is my main method which runs everything
+ * By Kareith Dyce
+ */
+
 #include "vm_sim.h"
 #include <string>
+#include <vector>
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
 
-using namespace std;
- /*TLB and pagetable*/
-tlb_t tlb;
-page_table_t pageTable;
 
+
+using namespace std;
+string backing_store = "BACKING_STORE";
+float pageFaults = 0;
+float tlbHits = 0;
 
 /* TLB */
 int initTLB(tlb_t &t){
@@ -42,7 +50,8 @@ int TLB_replacement_FIFO(page_t page, frame_t frame, tlb_t &t){
     return 0;
 }
 
-int searchTLB(page_t page, tlb_t t, frame_t &frame){
+
+int searchTLB(page_t page, tlb_t &t, frame_t &frame, page_table_t &table){
     int i = 0;
     for(i; i < TLB_SIZE; i++){
         if(!t.tlb_entry[i].valid){
@@ -50,32 +59,76 @@ int searchTLB(page_t page, tlb_t t, frame_t &frame){
         }
         if(t.tlb_entry[i].page_num == page){
             frame= t.tlb_entry[i].frame_num;
+            tlbHits++;
             return 0;
         }
     }
-    search_pageTable(page, pageTable, frame);
-    return 0;
+    return search_pageTable(page, t, frame, table);
+}
+
+
+
+int pageFaultHandler(page_t page, tlb_t &t, frame_t &frame, page_table_t &table){
+    cout<<"Page Fault"<<endl;
+    pageFaults++;
+    FILE *file = fopen((char*)backing_store.c_str(), "r");
+    byte_t oneByte[256];
+    fpos_t pos;
+    if(file == 0){
+        printf("Could not open file");
+    }
+    else{
+        fseek(file, page*PAGE_SIZE, SEEK_SET);
+        fgetpos(file, &pos);
+        //printf("Reading from position: %d.\n", pos);
+        int i = 0;
+        //for(i; i < PAGE_SIZE; i++){
+        fread(&oneByte, 1, 1, file);
+        TLB_replacement_FIFO(page, frame, t);
+        pageTableUpdate(page, frame, table);
+
+        //printf("%d", oneByte);
+        //}
+
+    }
+
+    fclose(file);
+    return searchTLB(page, t, frame, table);
 }
 
 
 
 /* Page Table*/
 int init_pageTable(page_table_t &pageTable){
-    return 0; 
+   unsigned int i;
+    pageTable.next_ptr = 0;
+    for(i = 0; i < NUM_PAGES; i++){
+        pageTable.pages[i].valid = false;
+    } 
+    return 0;  
 } 
 
 
-int search_pageTable(page_t page, page_table_t table, frame_t &frame){
+int search_pageTable(page_t page, tlb_t &t, frame_t &frame, page_table_t &table){
     int i = 0;
     for(i; i < PAGE_SIZE; i++){
-        if(table.pages[i] == page){
-            //frame = tlb.tlb_entry[i].frame_num;
+        if(!table.pages[i].valid){
+            break;
+        }
+        if(table.pages[i].page_num == page){
+            frame = table.pages[i].frame_num;
             return 0;
         }
     }
+    return pageFaultHandler(page, t, frame, table);
+    }
+
+int pageTableUpdate(page_t page, frame_t frame, page_table_t  &table){
+    table.pages[table.next_ptr].page_num = page;
+    table.pages[table.next_ptr].frame_num = frame;
+    table.next_ptr++;
     return 0;
 }
-
 
 /* Logic Address */
 int laddressLoader(string file, laddress_list_t &list){
@@ -85,12 +138,13 @@ int laddressLoader(string file, laddress_list_t &list){
         cout<<"Error!"<<endl;
         return -1;
     }
-    list.size = 0;
     int value;
     while(read>>value){
-        list.laddress[list.size] = value;
-        list.size++;
+        list.aList.push_back(value);
     }
+       
+
+    read.close();
     return 0;
 }
 
@@ -101,32 +155,37 @@ int extract_laddress(laddress_t laddress, page_t &page, offset_t &offset){
     return 0;
 }
 
+int create_paddress(frame_t frame_num, offset_t offset, paddress_t &paddress){
+    paddress = frame_num << OFFSET_BITS | offset;
+    return 0;
+}
 
 int main(){
     /*Set up Variables*/
     page_t page_num;
     frame_t frame_num;
     offset_t offset;
-    
+
+    /*TLB and pagetable*/
+    tlb_t tlb;
+    page_table_t pageTable;
+
+
     /*Addresses*/
     paddress_t paddress;
     laddress_t laddress;
     laddress_list_t laddressList;
     
    
-    //physical_memory_t physical_memory;
+    physical_memory_t physical_memory;
     
     /*Value and address-value list*/
     //value_t value;
     //address_value_list_t address_value_list;
 
-    /*Boolean for TLB hit and pgae fault*/
-    bool is_tlb_hit;
-    bool is_page_fault;
-    
     /*Input and Output file names*/
     string input_file = "testinput.txt";
-    string output_file = "output_physical_address_value";
+    string output_file = "testoutput.txt";
 
     /*Initialize the system*/
     initTLB(tlb);
@@ -135,12 +194,15 @@ int main(){
     /* Create a logical address list from the file*/
     laddressLoader(input_file, laddressList);
     int i = 0;
-    for(i; i < TLB_SIZE; i++){
-        extract_laddress(laddressList.laddress[i], page_num, offset);
-        searchTLB(page_num, tlb, frame_num);
+    float size = laddressList.aList.size();
+    for(i; i < size; i++){
+        extract_laddress(laddressList.aList.at(i), page_num, offset);
+        printf("laddress: %d page: %d offset: %d\n", laddressList.aList.at(i), page_num, offset);
+        searchTLB(page_num, tlb, frame_num, pageTable);
+        create_paddress(frame_num,offset, paddress);
     }
-
-    
-
+    cout<<"ALL Done"<<endl;
+    cout<<"Page fault rate: "<<pageFaults/(float)(size)<<" %"<<endl;
+    cout<<"TLB hit rate: "<<tlbHits/(float)size<<" %"<<endl;
     return 0;
 }
--- OS/Project6/vm_sim.h
+++ OS/Project6/vm_sim.h
@@ -1,4 +1,12 @@
+/** 
+ * Project 6
+ * This is my header file which defines all
+ * the functions
+ * By Kareith Dyce
+ */
+
 #include <string>
+#include <vector>
 #define OFFSET_BITS 8
 #define OFFSET_MASK 0xFF
 #define NUM_PAGES 256
@@ -6,7 +14,6 @@
 #define PAGE_SIZE 256
 #define TLB_SIZE 16
 #define PHYS_MEM 65536
-#define backing_store "BACKING_STORE"
 using namespace std;
 
 typedef unsigned int laddress_t;
@@ -32,35 +39,45 @@ typedef struct {
 
 
 typedef struct {    
-    laddress_t laddress[NUM_PAGES];
+    vector<laddress_t> aList;
     /* next candidate entry to be replaced/used */
     unsigned int size;
 }laddress_list_t;
 
 
 typedef struct {    
-    page_t pages[NUM_PAGES];
+    tlb_entry_t pages[NUM_PAGES];
+    //vector<int> pageHit = vector<int>(256,0);
     /* next candidate entry to be replaced/used */
     unsigned int next_ptr;
 }page_table_t;
 
+typedef struct {
+    paddress_t frame[NUM_FRAMES];
+    /* next candidate entry to be replaced/used */
+    unsigned int next_ptr;
+}physical_memory_t;
+
+
 /* TLB */
 int initTLB(tlb_t &t);
-int searchTLB(page_t page, tlb_t t, frame_t &frame);
+int searchTLB(page_t page, tlb_t &t, frame_t &frame, page_table_t &table);
 int TLB_display(tlb_t tlb);
 int TLB_replacement_FIFO(page_t page_num, frame_t frame_num, tlb_t  &t);
 
 /* Page Table*/
 int init_pageTable(page_table_t &pageTable);
-int search_pageTable(page_t page, page_table_t table, frame_t &frame_num);
+int search_pageTable(page_t page, tlb_t &t, frame_t &frame, page_table_t &table);
+int pageTableUpdate(page_t page, frame_t frame, page_table_t  &table);
+
 
 /* Logic Address*/
 int laddressLoader(string file, laddress_list_t &list);
 int extract_laddress(laddress_t laddress, page_t &page_num, offset_t &offset);
 
-int pageFaultHandler(page_t page_num, tlb_t &t);
+int pageFaultHandler(page_t page, tlb_t &t, frame_t &frame, page_table_t &table);
                                                                                                                              
 /* Physical Address*/
 int create_paddress(frame_t frame_num, offset_t offset, paddress_t &paddress);
-int read_paddress(paddress_t paddress, int value);
+//int read_paddress(paddress_t paddress, int value);
 
